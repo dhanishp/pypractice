c1ccc7808f648e3284617c2cf67a8ec0b42a73b7
dhanishp <106110376+dhanishp@users.noreply.github.com>
2022-05-24 20:01:53 -0500

    Add files via upload

---
+++ todolist.py
@@ -0,0 +1,44 @@
+
+todolist = ["homework","chores","dinner"]
+
+def checktodo():
+    print "\nTo-do:"
+    for x in todolist:
+        print(x)
+        
+def addtodo():
+    add = input("What do you want to add?")
+    todolist.append(add)
+    print "\nTo-do:"
+    for x in todolist:
+        print(x)
+       
+def removetodo():
+    remove = input("What do you want to remove?")
+
+    todolist.remove(remove)
+    print "\nTo-do:"
+    for x in todolist:
+        print(x)
+       
+i = 1
+
+while i < 3:
+    print ("\nWelcome to my todo list program, you may 'add','remove', 'check', or 'leave' todo") 
+    print ("\nMake sure to include all input into the program within quotation marks ")
+    command = input ("\nWhat would you like to do?\n")
+
+    if command == "print":
+        checktodo()
+
+    elif command == "add":
+        addtodo()
+
+    elif command == "remove":
+        removetodo()
+
+    elif command == "leave":
+        i += 10
+
+    else:
+        print ("\nPlease Enter a valid command")
